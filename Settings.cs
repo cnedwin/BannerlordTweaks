8359380871762eb2a8d0fb7a604a62ef8d8ca09d
Edwin Chen <cnedwin@gmail.com>
2021-01-28 13:16:37 +0800

    1.5.6 Update

--- Settings.cs
+++ Settings.cs
@@ -9,7 +9,7 @@ namespace BannerlordTweaks
     public class BannerlordTweaksSettings : AttributeGlobalSettings<BannerlordTweaksSettings>
     {
         public override string Id { get; } = "BannerlordTweaksSettings";
-        public override string DisplayName => "综合设置1.5.6.6 (cnedwin)";
+        public override string DisplayName => "综合设置1.5.6.7 (cnedwin)";
         public override string FolderName { get; } = "BannerlordTweaksSettings";
         public override string FormatType { get; } = "json2";
 
